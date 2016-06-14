5614bee7ab8727858d6f18e148888f8417fadf40
Milos Havlicek <miloshavlicek@gmail.com>
2016-06-14 09:50:14 +0200

    + CookiesOK support

--- src/Bar.php
+++ src/Bar.php
@@ -3,15 +3,23 @@
 namespace Cothema\EuCookies;
 
 class Bar extends \Nette\Application\UI\Control {
-    
+
     public $moreInfoLink = 'https://www.google.com/policies/technologies/cookies/';
 
     public function render() {
         $template = $this->template;
         $template->setFile(__DIR__ . '/Bar.latte');
         $template->moreInfoLink = $this->moreInfoLink;
+        $template->alreadyConfirmed = $this->isAlreadyConfirmed();
 
         $template->render();
+    }
+
+    private function isAlreadyConfirmed() {
+        if (isset($_SERVER["HTTP-X-COOKIESOK"])) {
+            return TRUE;
+        }
+        return FALSE;
     }
 
 }
